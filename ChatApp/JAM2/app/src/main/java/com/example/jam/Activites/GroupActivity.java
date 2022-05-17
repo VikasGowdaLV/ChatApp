b7fbafe7c7eb346aa52c556d1af83586c00ba144
VikasGowdaLV <vikas.vicky1947@gmail.com>
2022-05-18 02:46:21 +0530

    Final Commit

---
+++ ChatApp/JAM2/app/src/main/java/com/example/jam/Activites/GroupActivity.java
@@ -0,0 +1,18 @@
+package com.example.jam.Activites;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jam.R;
+
+public class GroupActivity  extends AppCompatActivity {
+    
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
